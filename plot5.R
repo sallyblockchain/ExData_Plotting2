d57832e3acbab2dd1e554b275c695baa44f081a1
Xiaodan Zhang <zhang349@illinois.edu>
2014-07-24 22:51:40 -0500

    Finished all codes and plots.

--- plot5.R
+++ plot5.R
@@ -1,16 +1,14 @@
 setwd("~/Desktop/Online Coursera/Coursera-Exploratory-Data-Analysis/ExData_Plotting2/")
 unzip("./data/exdata-data-NEI_data.zip", exdir = "./data/")
-# Check if both data exist. If not, load the data.
+# Check if both data exist in the environment. If not, load the data.
 if (!"neiData" %in% ls()) {
     neiData <- readRDS("./data/summarySCC_PM25.rds")
 }
 if (!"sccData" %in% ls()) {
     sccData <- readRDS("./data/Source_Classification_Code.rds")
 }
-head(neiData)
-head(sccData)
-dim(neiData) # 6497651   6
-dim(sccData) # 11717    15
+
+subset <- neiData[neiData$fips == "24510", ] 
 
 # if (!"load_data.R" %in% list.files()) {
 #     setwd("~/Desktop/Online Coursera/Coursera-Exploratory-Data-Analysis/ExData_Plotting2/")
@@ -18,14 +16,16 @@ dim(sccData) # 11717    15
 # source("load_data.R")
 
 par("mar"=c(5.1, 4.5, 4.1, 2.1))
-png(filename = "./figure/plot1.png", 
+png(filename = "./figure/plot5.png", 
     width = 480, height = 480, 
     units = "px")
-totalEmissions <- aggregate(neiData$Emissions, list(neiData$year), FUN = "sum")
-# options(scipen=0)
-# options(scipen=999)
-plot(totalEmissions, type = "l", xlab = "Year", 
-     main = "Total Emissions in the United States from 1999 to 2008", 
-     ylab = expression('Total PM'[2.5]*" Emission"))
-dev.off()
+motor <- grep("motor", sccData$Short.Name, ignore.case = T)
+motor <- sccData[motor, ]
+motor <- subset[subset$SCC %in% motor$SCC, ]
+motorEmissions <- aggregate(motor$Emissions, list(motor$year), FUN = "sum")
 
+plot(motorEmissions, type = "l", xlab = "Year", 
+     main = "Total Emissions From Motor Vehicle Sources\n from 1999 to 2008 in Baltimore City", 
+     ylab = expression('Total PM'[2.5]*" Emission"))
+
+dev.off()
